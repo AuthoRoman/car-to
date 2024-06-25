39b7daf203d99c2e1c113c62ebef2f42e0f7a7f6
AuthoRoman <kit.kiram2017@yandex.ru>
2024-06-25 19:18:05 +0300

    fixBugapp.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,8 @@
 import React, { useEffect, useState } from "react";
-import ReactDOM from "react-dom/client";
+ 
 import "./index.css";
 
-import { Provider } from "react-redux";
-import { store } from "./state/store";
+ 
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import WaitingsCarPage from "./pages/WaitingsCarPage";
 import CarinWorking from "./pages/CarInWorking";
