39e155a09508551568683671088078741a08b969
AuthoRoman <kit.kiram2017@yandex.ru>
2024-06-28 17:50:58 +0300

    addReStylesWithMUI

--- src/components/Popups/CreateCardWaitingsPopup/CreateCardPopup.tsx
+++ src/components/Popups/CreateCardWaitingsPopup/CreateCardPopup.tsx
@@ -8,21 +8,28 @@ import {
   FormGroup,
   FormLabel,
   Input,
+  TextField,
 } from "@mui/material";
 import { MuiTelInput } from "mui-tel-input";
 import { useTypedDispatch } from "../../../state/hooks/hooks";
 import { Color, TypeBases } from "../../../state/types";
 import { Unstable_NumberInput as NumberInput } from "@mui/base/Unstable_NumberInput";
 import { addData } from "../../../api/database/db";
+import { Height } from "@mui/icons-material";
 
 const CreateCardPopup: React.FC<{ closeVisible: any }> = ({ closeVisible }) => {
   const dispatch = useTypedDispatch();
+  const [nameError, setNameError] = useState<boolean>(false);
+  const [secondNameError, setSecondnameError] = useState<boolean>(false);
+  const [ownerNumbersError, setNumbersOwnersError] = useState<boolean>(false);
+  const [VINError, setVINError] = useState<boolean>(false);
+  const [emailError, setEmailError] = useState<boolean>(false);
 
   const [phone, setPhone] = useState("");
   const [email, setEmail] = useState("");
-  const [firstNameOwner, setFirstNameOwner] = useState("");
-  const [secondNameOwner, setSecondNameOwner] = useState("");
-  const [numberOwners, setNumberOwners] = useState<number>(0);
+  const [firstNameOwner, setFirstNameOwner] = useState<string>("");
+  const [secondNameOwner, setSecondNameOwner] = useState<string>("");
+  const [numberOwners, setNumberOwners] = useState<number>();
   const [color, setColor] = useState<Color | string>("");
   const [carMileage, setCarMileage] = useState("");
   const [registration, setRegistration] = useState("");
@@ -57,25 +64,35 @@ const CreateCardPopup: React.FC<{ closeVisible: any }> = ({ closeVisible }) => {
     alarm,
     steeringSystem,
   } = problems;
-  const IdKey =Math.random() * 100
+  const IdKey = Math.random() * 100;
   async function submitForm() {
-    await addData(TypeBases.CARS_IN_WAITING,   {
-      accidents: accidents,
-      carMileage: carMileage,
-      color: color,
-      email: email,
-      firstNameOwner: firstNameOwner,
-      id: IdKey,
-      numberOwners: numberOwners,
-      registration: registration,
-      secondNameOwner: secondNameOwner,
-      tel: phone,
-      VIN: VIN,
-      problems: problems,
-    } )
-    await dispatch({
-      type: "ADD_CAR",
-      payload: {
+    if (VIN.length !== 17) {
+      setVINError(true);
+    }
+    if (firstNameOwner.trim().length === 0) {
+      setNameError(true);
+    }
+    if (secondNameOwner.trim().length === 0) {
+      setSecondnameError(true);
+    }
+    if (
+      numberOwners! <= 0 ||
+      numberOwners === undefined ||
+      numberOwners === null
+    ) {
+      setNumbersOwnersError(true);
+    }
+
+    if (
+      VIN.length === 17 &&
+      firstNameOwner.trim().length !== 0 &&
+      secondNameOwner.trim().length !== 0 &&
+      (numberOwners! > 0 ||
+        numberOwners !== undefined ||
+        numberOwners !== null) &&
+      emailError === false
+    ) {
+      await addData(TypeBases.CARS_IN_WAITING, {
         accidents: accidents,
         carMileage: carMileage,
         color: color,
@@ -88,89 +105,161 @@ const CreateCardPopup: React.FC<{ closeVisible: any }> = ({ closeVisible }) => {
         tel: phone,
         VIN: VIN,
         problems: problems,
-      },
-    });
-    closeVisible(false);
+      });
+      await dispatch({
+        type: "ADD_CAR",
+        payload: {
+          accidents: accidents,
+          carMileage: carMileage,
+          color: color,
+          email: email,
+          firstNameOwner: firstNameOwner,
+          id: IdKey,
+          numberOwners: numberOwners!,
+          registration: registration,
+          secondNameOwner: secondNameOwner,
+          tel: phone,
+          VIN: VIN,
+          problems: problems,
+        },
+      });
+      closeVisible(false);
+    }
+    return 0;
   }
-  const handleNumberChange = (
-    event:
-      | React.FocusEvent<HTMLInputElement>
-      | React.PointerEvent<Element>
-      | React.KeyboardEvent<Element>,
-    value: number | null
+
+  const ValidationFirstNameOwner = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setFirstNameOwner(e.target.value);
+    setNameError(false);
+  };
+  const ValidationSecondNameOwner = (
+    e: React.ChangeEvent<HTMLInputElement>
   ) => {
-    event.preventDefault();
-    if (value !== null) {
-      setNumberOwners(value);
+    setSecondNameOwner(e.target.value);
+
+    setSecondnameError(false);
+  };
+  const ValidationVIN = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setVIN(e.target.value);
+    setVINError(false);
+  };
+  const ValidationNumberOwners = (
+    event: React.ChangeEvent<HTMLInputElement>
+  ) => {
+    setNumberOwners(+event.target.value);
+    setNumbersOwnersError(false);
+  };
+  const ValidationEmail = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const pattern = new RegExp(
+      /^(("[\w-\s]+")|([\w-]+(?:\.[\w-]+)*)|("[\w-\s]+")([\w-]+(?:\.[\w-]+)*))(@((?:[\w-]+\.)*\w[\w-]{0,66})\.([a-z]{2,6}(?:\.[a-z]{2})?)$)|(@\[?((25[0-5]\.|2[0-4][0-9]\.|1[0-9]{2}\.|[0-9]{1,2}\.))((25[0-5]|2[0-4][0-9]|1[0-9]{2}|[0-9]{1,2})\.){2}(25[0-5]|2[0-4][0-9]|1[0-9]{2}|[0-9]{1,2})\]?$)/i
+    );
+    setEmail(e.target.value);
+    if (pattern.test(email)) {
+      setEmailError(false);
     }
   };
+
   return (
     <div>
       <div className={styles.createCardPopup}>
         <form className={styles.form} action="">
           <h1>Форма для заполнения заявки</h1>
 
-          <Input className={styles.inputCustom}
+          <TextField
+            className={styles.inputPhoneCustom}
             value={firstNameOwner}
-            onChange={(e) => setFirstNameOwner(e.target.value)}
+            error={
+              nameError && firstNameOwner.trim().length === 0 ? true : false
+            }
+            helperText={
+              nameError && firstNameOwner.trim().length === 0
+                ? "Введите имя"
+                : false
+            }
+            onChange={ValidationFirstNameOwner}
+            variant="standard"
             sx={{
               padding: "0px 10px",
               backgroundColor: "white",
             }}
-            placeholder="Name"
+            placeholder="Имя"
             color="warning"
           />
 
-          <Input className={styles.inputCustom}
+          <TextField
+            className={styles.inputPhoneCustom}
             value={secondNameOwner}
-            onChange={(e) => setSecondNameOwner(e.target.value)}
+            error={
+              secondNameError && secondNameOwner.trim().length === 0
+                ? true
+                : false
+            }
+            helperText={
+              secondNameError && secondNameOwner.trim().length === 0
+                ? "Введите фамилию"
+                : false
+            }
+            onChange={ValidationSecondNameOwner}
+            variant="standard"
             sx={{
               padding: "0px 10px",
               backgroundColor: "white",
             }}
-            placeholder="SecondName"
+            placeholder="Фамилия"
             color="warning"
           />
-          <MuiTelInput className={styles.inputPhoneCustom}
+          <MuiTelInput
+            className={styles.inputPhoneCustom}
             value={phone}
             onChange={handlePhone}
             sx={{
               backgroundColor: "white",
             }}
-            placeholder="Your phone"
+            placeholder="Телефон"
             color="warning"
           />
-          <Input className={styles.inputCustom}
+          <TextField
+            className={styles.inputPhoneCustom}
             value={VIN}
-            onChange={(e) => setVIN(e.target.value)}
+            error={VINError && VIN.trim().length !== 17 ? true : false}
+            helperText={
+              VINError && VIN.trim().length !== 17
+                ? "Введите 17 символов VIN"
+                : false
+            }
+            onChange={ValidationVIN}
+            variant="standard"
             sx={{
               padding: "0px 10px",
               backgroundColor: "white",
             }}
-            placeholder="VIN number 17 numbers"
+            placeholder="VIN 17 символов (обязательно)"
             color="warning"
           />
-          <Input className={styles.inputCustom}
+          <Input
+            className={styles.inputCustom}
             value={registration}
             onChange={(e) => setRegistration(e.target.value)}
             sx={{
               padding: "0px 10px",
               backgroundColor: "white",
             }}
-            placeholder="registration"
+            placeholder="Регистрирована"
             color="warning"
           />
-          <Input className={styles.inputCustom}
+          <Input
+            className={styles.inputCustom}
             value={carMileage}
             onChange={(e) => setCarMileage(e.target.value)}
             sx={{
               padding: "0px 10px",
               backgroundColor: "white",
             }}
-            placeholder="carMileage"
+            placeholder="Пробег автомобиля"
             color="warning"
           />
-          <Input className={styles.inputCustom}
+          <Input
+            className={styles.inputCustom}
             value={color}
             onChange={(e) => setColor(e.target.value)}
             sx={{
@@ -178,41 +267,75 @@ const CreateCardPopup: React.FC<{ closeVisible: any }> = ({ closeVisible }) => {
               backgroundColor: "white",
             }}
             color="warning"
-             
-            placeholder="color"
+            placeholder="Цвет"
           />
-          <NumberInput className={styles.inputCustom}
-            aria-label="Demo number input"
+          <TextField
+            className={styles.inputPhoneCustom}
+            error={
+              ownerNumbersError &&
+              (numberOwners === 0 ||
+                numberOwners === undefined ||
+                numberOwners === null)
+                ? true
+                : false
+            }
+            helperText={
+              ownerNumbersError &&
+              (numberOwners === 0 ||
+                numberOwners === undefined ||
+                numberOwners === null)
+                ? "Введите корректное число владельцев"
+                : false
+            }
+            onChange={ValidationNumberOwners}
+            placeholder="Количество владельцев"
+            variant="standard"
             value={numberOwners}
-            onChange={handleNumberChange}
-            component="input"
-            placeholder="numberOwners"
             color="warning"
+            inputProps={{ type: "number" }}
+            sx={{
+              padding: "0px 10px",
+              backgroundColor: "white",
+            }}
           />
 
-          <Input className={styles.inputCustom}
+          <Input
+            className={styles.inputCustom}
             value={accidents}
             onChange={(e) => setAccidents(e.target.value)}
             sx={{
               padding: "0px 10px",
               backgroundColor: "white",
             }}
-            placeholder="accidents"
+            placeholder="Аварии"
             color="warning"
           />
-          <Input className={styles.inputCustom}
+          <TextField
+            className={styles.inputPhoneCustom}
             value={email}
-            onChange={(e) => setEmail(e.target.value)}
+            onChange={ValidationEmail}
+             variant="standard"
+            error={emailError && email.trim().length !== 0 ? true : false}
+           
+            helperText={
+              emailError && email.trim().length !== 0
+                ? "Введите корректную электронную почту"
+                : false                                           //TODO
+            }
             sx={{
               padding: "0px 10px",
               backgroundColor: "white",
             }}
-            placeholder="email"
+            placeholder="Электронная почта"
             color="warning"
           />
+
           <FormControl>
-            <FormLabel color="warning" aria-labelledby="demo-radio-buttons-group-label">
-              Problems
+            <FormLabel
+              color="warning"
+              aria-labelledby="demo-radio-buttons-group-label"
+            >
+              Проблемы вашего авто
             </FormLabel>
             <FormGroup
               className={styles.createCardPopup__radioGroup}
@@ -224,58 +347,110 @@ const CreateCardPopup: React.FC<{ closeVisible: any }> = ({ closeVisible }) => {
               aria-labelledby="demo-radio-buttons-group-label"
               defaultValue="female"
             >
-              <FormControlLabel className={styles.checkBoxLabelCustom}
+              <FormControlLabel
+                className={styles.checkBoxLabelCustom}
                 value="engine"
-                control={<Checkbox checked={engine} onChange={handleChange}  name='engine' color="warning"/>}
-                label="engine"
+                control={
+                  <Checkbox
+                    checked={engine}
+                    onChange={handleChange}
+                    name="engine"
+                    color="warning"
+                  />
+                }
+                label="Двигатель"
               />
-              <FormControlLabel  className={styles.checkBoxLabelCustom}
+              <FormControlLabel
+                className={styles.checkBoxLabelCustom}
                 value="fuses"
-                control={<Checkbox checked={fuses} onChange={handleChange}  name='fuses' color="warning"/>}
-                label="fuses"
+                control={
+                  <Checkbox
+                    checked={fuses}
+                    onChange={handleChange}
+                    name="fuses"
+                    color="warning"
+                  />
+                }
+                label="Предохранители"
               />
-              <FormControlLabel className={styles.checkBoxLabelCustom}
+              <FormControlLabel
+                className={styles.checkBoxLabelCustom}
                 value="catalyst"
                 control={
-                  <Checkbox checked={catalyst} onChange={handleChange}  name='catalyst' color="warning"/>
+                  <Checkbox
+                    checked={catalyst}
+                    onChange={handleChange}
+                    name="catalyst"
+                    color="warning"
+                  />
                 }
-                label="catalyst"
+                label="Катализатор"
               />
-              <FormControlLabel className={styles.checkBoxLabelCustom}
+              <FormControlLabel
+                className={styles.checkBoxLabelCustom}
                 value="generator"
-                control={<Checkbox checked={generator}  onChange={handleChange} name='generator' color="warning"/>}
-                label="generator"
+                control={
+                  <Checkbox
+                    checked={generator}
+                    onChange={handleChange}
+                    name="generator"
+                    color="warning"
+                  />
+                }
+                label="Генератор"
               />
-              <FormControlLabel className={styles.checkBoxLabelCustom}
+              <FormControlLabel
+                className={styles.checkBoxLabelCustom}
                 value="brakeSystem"
                 control={
-                  <Checkbox checked={brakeSystem} onChange={handleChange}  name='brakeSystem' color="warning"/>
+                  <Checkbox
+                    checked={brakeSystem}
+                    onChange={handleChange}
+                    name="brakeSystem"
+                    color="warning"
+                  />
                 }
-                label="brakeSystem"
+                label="Тормозная система"
               />
-              <FormControlLabel className={styles.checkBoxLabelCustom}
+              <FormControlLabel
+                className={styles.checkBoxLabelCustom}
                 value="windshieldWashers"
                 control={
                   <Checkbox
                     checked={windshieldWashers}
-                    onChange={handleChange} name='windshieldWashers'
+                    onChange={handleChange}
+                    name="windshieldWashers"
                     color="warning"
                   />
                 }
-                label="windshieldWashers"
+                label="Омыватели лобового стекла"
               />
 
-              <FormControlLabel className={styles.checkBoxLabelCustom}
+              <FormControlLabel
+                className={styles.checkBoxLabelCustom}
                 value="alarm"
-                control={<Checkbox checked={alarm} onChange={handleChange}  name='alarm' color="warning"/>}
-                label="alarm"
+                control={
+                  <Checkbox
+                    checked={alarm}
+                    onChange={handleChange}
+                    name="alarm"
+                    color="warning"
+                  />
+                }
+                label="Аварийная сигнализация"
               />
-              <FormControlLabel className={styles.checkBoxLabelCustom}
+              <FormControlLabel
+                className={styles.checkBoxLabelCustom}
                 value="steeringSystem"
                 control={
-                  <Checkbox checked={steeringSystem} onChange={handleChange} name='steeringSystem' color="warning" />
+                  <Checkbox
+                    checked={steeringSystem}
+                    onChange={handleChange}
+                    name="steeringSystem"
+                    color="warning"
+                  />
                 }
-                label="steeringSystem"
+                label="Рулевая система"
               />
             </FormGroup>
           </FormControl>
@@ -289,12 +464,10 @@ const CreateCardPopup: React.FC<{ closeVisible: any }> = ({ closeVisible }) => {
                 },
               }}
               variant="contained"
-               
             >
               Cancel
             </Button>
             <Button
-            disabled ={!(VIN.length===17)}
               onClick={submitForm}
               sx={{
                 backgroundColor: "#e68d1a",
--- src/pages/Layout/Layout.tsx
+++ src/pages/Layout/Layout.tsx
@@ -1,34 +1,84 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { Link, useLocation, useNavigate } from "react-router-dom";
 import styles from "./Layout.module.css";
+import { Tab, Tabs } from "@mui/material";
 
 interface LayoutProps {
   children: any;
 }
 
-const Layout: React.FC<LayoutProps> = ({ children }) => (
-  <div>
-    <nav>
-      <ul className={styles.navList}>
-        <li>
-          <Link className={styles.navList__item} to="/car-to">
-            Очередь на обслуживание
-          </Link>
-        </li>
-        <li>
-          <Link className={styles.navList__item} to="/inwork">
-            Машины обслуживаются
-          </Link>
-        </li>
-        <li>
-          <Link className={styles.navList__item} to="/finish">
-            Машины, которые прошли обслуживание
-          </Link>
-        </li>
-      </ul>
-    </nav>
-    <main>{children}</main>
-  </div>
-);
+const Layout: React.FC<LayoutProps> = ({
+  children,
+}: {
+  children: React.ReactNode;
+}) => {
+  const navigate = useNavigate();
+  console.log(navigate);
+  const location = useLocation();
+  console.log(location);
+  const currentTab = location.pathname;
 
+  const handlerChange = (e: React.SyntheticEvent, newValue: string) => {
+    navigate(newValue);
+  };
+
+  return (
+    <div>
+      <nav>
+        <Tabs
+          value={currentTab}
+          onChange={handlerChange}
+          textColor="secondary"
+          indicatorColor="secondary"
+          aria-label="secondary tabs example"
+          className={styles.navList }
+          centered
+        >
+          <Tab
+            label=" Очередь на обслуживание"
+            /*className={styles.navList__item}*/ value="/car-to"
+          />
+          <Tab
+            label=" Машины обслуживаются"
+            /*className={styles.navList__item}*/ value="/inwork"
+          />
+          <Tab
+            label=" Машины, которые прошли обслуживание"
+            /*className={styles.navList__item}*/ value="/finish"
+          />
+        </Tabs>
+      </nav>
+      <main>{children}</main>
+    </div>
+  );
+};
 export default Layout;
+// <TabContext value={value}>
+{
+  /* <Box sx={{ borderBottom: 1, borderColor: "divider" }}>
+<Box sx={{ borderBottom: 1, borderColor: "divider" }}>
+<TabList onChange={handleChange} aria-label="lab API tabs example">
+    <Tab label="Очередь на обслуживание" value="1" />
+    <Tab label=" Машины обслуживаются" value="2" />
+    <Tab label="Машины, которые прошли обслуживание" value="3" />
+  </TabList>
+</Box>
+
+<TabPanel value="1">
+  <Link className={styles.navList__item} to="/car-to">
+    Очередь на обслуживание
+  </Link>
+</TabPanel>
+<TabPanel value="2">
+  <Link className={styles.navList__item} to="/inwork">
+    Машины обслуживаются
+  </Link>
+</TabPanel>
+<TabPanel value="3">
+  <Link className={styles.navList__item} to="/finish">
+    Машины, которые прошли обслуживание
+  </Link>
+</TabPanel>
+</Box>
+</TabContext> */
+}
