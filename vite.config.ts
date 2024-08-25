e042402f4054eb5a9f59b9d76176c80a43676dba
AuthoRoman <kit.kiram2017@yandex.ru>
2024-08-25 10:41:29 +0300

    bugfix: *dont published

--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,11 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 
 export default defineConfig({
-  base: "/car-to/",
+  base: "/",
   plugins: [react()],
+
   build: {
-    outDir: "../build",
+    outDir: "./build",
     emptyOutDir: true, // also necessary
   },
 });
